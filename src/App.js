bcf63d7c72c896f046c3164e73cd62d0ee41680c
Aysel552 <a.abbasaliyeva1992@gmail.com>
2022-01-19 01:08:41 +0400

    added url

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import { Component } from 'react';
+
+
+class App extends Component {
+
+  state = {
+    person: null
+  }
+
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({person: data.results[0]})
+}
+
+  render() {
+    return (
+      <div>
+        {!this.state.person ? <p>Loading ...</p> :
+          <div>
+            <p>First Name: {this.state.person.name.first}</p>
+            <p>Last Name: {this.state.person.name.last}</p>
+            <p>Email: {this.state.person.email}</p>
+            <img src={this.state.person.picture.large} alt='Human' width="400px"/>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+
+  
+
+export default App;
